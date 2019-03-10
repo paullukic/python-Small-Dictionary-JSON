877de1d0e2a3391a2893fbfdee39fc5731aa9694
Paul <48401847+ultimatus1@users.noreply.github.com>
2019-03-10 08:32:34 +0100

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,18 +1,18 @@
 import json
 from difflib import get_close_matches
 
-data = json.load(open('JSON\dictionary.json'))
+data = json.load(open('JSON\dictionary.json'))  # load your JSON dictionary
 
 
-def definition(w):
-    w = w.lower()
-    if w in data:
+def definition(w):  # function for finding a word
+    w = w.lower()  # making command case insensitive 
+    if w in data:  # if word is correct
         return data[w]
     elif w.title() in data:  # if user entered "texas" this will check for "Texas" as well.
         return data[w.title()]
     elif w.upper() in data:  # in case user enters words like USA or NATO
         return data[w.upper()]
-    elif len(get_close_matches(w, data.keys())) > 0:
+    elif len(get_close_matches(w, data.keys())) > 0:  # find close matches and print them
         print('Did you mean:')
         matches = get_close_matches(w, data.keys())
         for i in matches:
@@ -22,13 +22,13 @@ def definition(w):
         return "The word doesn't exist, please double check it."
 
 
-command = ''
-while command != 'q' or 'Q':
-    command = command.lower()
+command = ''  # empty string for commands
+while command != 'q' or 'Q':  
+    command = command.lower()  # making command case insensitive 
     print('''To enter a word type E
 To quit type Q''')
     command = input('>>>')
-    if command == 'e':
+    if command == 'e':  # going thro commands
         word = input('Enter a word: ')
         output = definition(word)
         if type(output) == list:
